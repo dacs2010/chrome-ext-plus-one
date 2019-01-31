e6afe4e044ce27a9ca2ba00c50893e14f14fd863
xXDuckXx <dacs2010@gmail.com>
2019-01-31 14:09:52 -0500

    gets url, and updates the fcurrent tabs url

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.tabs.getS
-});
+console.log("connected")
+
+// chrome.runtime.onClicked.addListener(function (tab) {
+//     chrome.tabs.query({active: true,  currentWindow: true}, function() {
+//         console.log(tabs[0].url)
+//     })
+// })
+ 
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function	(tabs) {
+        let tabUrl = []
+        let curTab = tabs[0]
+        console.log(curTab.url)
+        let newUrl = tabUrl.toString()
+        chrome.tabs.update({url: newUrl})
+    })
+})
